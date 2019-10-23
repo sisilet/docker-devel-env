bab6f448a699c75b63e62c05b895d35316ba41d9
Ubuntu <ubuntu@ip-172-31-20-204.ap-southeast-2.compute.internal>
2019-10-23 10:35:14 +0000

    verified changes

--- conan-1.19/conan_add_repositories.sh
+++ conan-1.19/conan_add_repositories.sh
@@ -2,6 +2,6 @@
 
 # Add conan repositories of conan base module for eclipse user
 chroot --userspec=$USER_NAME / \
-  conan remote add braintwister https://api.bintray.com/conan/braintwister/conan 
   conan remote add conan-community https://api.bintray.com/conan/conan-community/conan
   conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
+  conan remote add braintwister https://api.bintray.com/conan/braintwister/conan 
